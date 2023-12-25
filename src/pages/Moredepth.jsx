d5ca30e86bf5448be52873aa4079a7a03e3367c4
Abhijith M R <abhijithmr581@gmail.com>
2023-12-25 12:53:04 +0530

    integrating google Map

---
+++ src/pages/Moredepth.jsx
@@ -0,0 +1,72 @@
+import {
+    GoogleMap,
+    InfoWindow,
+    Marker,
+    MarkerF,
+    useLoadScript,
+  } from "@react-google-maps/api";
+  import { useState } from "react";
+  
+  const MoreDepth = () => {
+    const { isLoaded } = useLoadScript({
+      googleMapsApiKey: 'AIzaSyC68fyrb-DrJL7hr3POLu-oNj271iwU-_A',
+    });
+    const [mapRef, setMapRef] = useState();
+    const [isOpen, setIsOpen] = useState(false);
+    const [infoWindowData, setInfoWindowData] = useState();
+    const markers = [
+      { address: "Address1", lat: 18.5204, lng: 73.8567 },
+      { address: "AddrEss2", lat: 18.5314, lng: 73.8446 },
+      { address: "Address3", lat: 18.5642, lng: 73.7769 },
+    ];
+  
+    const onMapLoad = (map) => {
+      setMapRef(map);
+      const bounds = new google.maps.LatLngBounds();
+      markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+      map.fitBounds(bounds);
+    };
+  
+    const handleMarkerClick = (id, lat, lng, address) => {
+      mapRef?.panTo({ lat, lng });
+      setInfoWindowData({ id, address });
+      setIsOpen(true);
+      console.log(infoWindowData);
+    };
+  
+    return (
+      <div className="App">
+        {!isLoaded ? (
+          <h1>Loading...</h1>
+        ) : (
+          <GoogleMap
+            mapContainerClassName="map-container"
+            onLoad={onMapLoad}
+            onClick={() => setIsOpen(false)}
+          >
+            {markers.map(({ address, lat, lng }, ind) => (
+              <MarkerF
+                key={ind}
+                position={{ lat, lng }}
+                onClick={() => {
+                  handleMarkerClick(ind, lat, lng, address);
+                }}
+              >
+                {isOpen && infoWindowData?.id === ind && (
+                  <InfoWindow
+                    onCloseClick={() => {
+                      setIsOpen(false);
+                    }}
+                  >
+                    <h3>{infoWindowData.address}</h3>
+                  </InfoWindow>
+                )}
+              </MarkerF>
+            ))}
+          </GoogleMap>
+        )}
+      </div>
+    );
+  };
+  
+  export default MoreDepth;
